4e3cf54380b98f447912eba4d7536d04eecb034c
Kevin Miao <wkmiao@edu.uwaterloo.ca>
2018-08-25 09:11:41 -0400

    frontend template

---
+++ flask-vuejs/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, jsonify, request
+from datetime import datetime
+
+
+class CustomFlask(Flask):
+    jinja_options = Flask.jinja_options.copy()
+    jinja_options.update(dict(
+      block_start_string='{%',
+      block_end_string='%}',
+      variable_start_string='((',
+      variable_end_string='))',
+      comment_start_string='{#',
+      comment_end_string='#}',
+    ))
+
+
+app = CustomFlask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/example')
+def example():
+    message = "Hello Flask!"
+    return render_template('example.html', message=message)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
