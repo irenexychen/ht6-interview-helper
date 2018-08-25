e0d6228bc70b2cda6c663d5621ba6bcbd4b65e30
PotentialOnWings <xiangyi99@live.com>
2018-08-25 17:30:43 -0400

    reformat

--- flask-vuejs/app.py
+++ flask-vuejs/app.py
@@ -13,18 +13,25 @@ class CustomFlask(Flask):
       comment_end_string='#}',
     ))
 
-
 app = CustomFlask(__name__)
+
+#pages
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
 @app.route('/example')
 def example():
     message = "Hello Flask!"
     return render_template('example.html', message=message)
+
+@app.route('/result_summary')
+def result_summary():
+    return render_template('example.html', message=message)
+
+
 
 
 if __name__ == '__main__':
