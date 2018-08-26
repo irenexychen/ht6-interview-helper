06d79fcf8ae0be0f9f0dc9513810bd3863b3c51e
Kevin Miao <wkmiao@edu.uwaterloo.ca>
2018-08-26 03:59:13 -0400

    removed record

--- basic-flask-app/camera.py
+++ basic-flask-app/camera.py
@@ -1,80 +1,22 @@
 import cv2
-import threading
-
-class RecordingThread (threading.Thread):
-    def __init__(self, name, camera):
-        threading.Thread.__init__(self)
-        self.name = name
-        self.isRunning = True
-
-        self.cap = camera
-        fourcc = cv2.VideoWriter_fourcc(*'H264')
-        self.out = cv2.VideoWriter('./static/video.mp4',fourcc, 30.0, (int(self.cap.get(3)), int(self.cap.get(4))))
-
-    def run(self):
-        while self.isRunning:
-            ret, frame = self.cap.read()
-            if ret:
-                self.out.write(frame)
-
-        self.out.release()
-
-    def stop(self):
-        self.isRunning = False
-
-    def __del__(self):
-        self.out.release()
 
 class VideoCamera(object):
     def __init__(self):
-        # Open a camera
-        self.cap = cv2.VideoCapture(0)
-      
-        # Initialize video recording environment
-        self.is_record = False
-        self.out = None
-
-        # Thread for recording
-        self.recordingThread = None
+        # Using OpenCV to capture from device 0. If you have trouble capturing
+        # from a webcam, comment the line below out and use a video file
+        # instead.
+        self.video = cv2.VideoCapture(0)
+        # If you decide to use video.mp4, you must have this file in the folder
+        # as the main.py.
+        # self.video = cv2.VideoCapture('video.mp4')
     
     def __del__(self):
-        self.cap.release()
+        self.video.release()
     
     def get_frame(self):
-        ret, frame = self.cap.read()
-
-        if ret:
-            ret, jpeg = cv2.imencode('.jpg', frame)
-
-            # Record video
-            # if self.is_record:
-            #     if self.out == None:
-            #         fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-            #         self.out = cv2.VideoWriter('./static/video.mp4',fourcc, 20.0, (640,480))
-                
-            #     ret, frame = self.cap.read()
-            #     if ret:
-            #         self.out.write(frame)
-            # else:
-            #     if self.out != None:
-            #         self.out.release()
-            #         self.out = None  
-
-            return jpeg.tobytes()
-      
-        else:
-            return None
-
-    def start_record(self):
-        self.is_record = True
-        self.recordingThread = RecordingThread("Video Recording Thread", self.cap)
-        self.recordingThread.daemon = True
-        self.recordingThread.start()
-
-    def stop_record(self):
-        self.is_record = False
-
-        if self.recordingThread != None:
-            self.recordingThread.stop()
-
-            
+        success, image = self.video.read()
+        # We are using Motion JPEG, but OpenCV defaults to capture raw images,
+        # so we must encode it into JPEG in order to correctly display the
+        # video stream.
+        ret, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
--- basic-flask-app/routes.py
+++ basic-flask-app/routes.py
@@ -2,8 +2,6 @@ from flask import Flask, render_template, Response, jsonify, request
 from camera import VideoCamera
 import cv2
 
-video_camera = None
-global_frame = None
 
 app = Flask(__name__)
 
@@ -11,67 +9,16 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', the_title='ElevAIte')
 
-@app.route('/register')
-def register():
-    return render_template('register.html')
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
-# @app.route('/camera')
-# def camera():
-# 	return Response(video_stream(),
-#                     mimetype='multipart/x-mixed-replace; boundary=frame')
-
-@app.route('/analysis')
-def analysis():
-    return render_template('analysis.html')
-
-
-@app.route('/camera')
-def camera():
-    return render_template('camera.html', the_title='WebcamTest')
-
-@app.route('/record_status', methods=['POST'])
-def record_status():
-    global video_camera 
-    if video_camera == None:
-        video_camera = VideoCamera()
-
-    json = request.get_json()
-
-    status = json['status']
-
-    if status == "true":
-        video_camera.start_record()
-        return jsonify(result="started")
-    else:
-        video_camera.stop_record()
-        return jsonify(result="stopped")
-
-def video_stream():
-    global video_camera 
-    global global_frame
-
-    if video_camera == None:
-        video_camera = VideoCamera()
-        
+def gen(camera):
     while True:
-        frame = video_camera.get_frame()
-
-        if frame != None:
-            global_frame = frame
-            yield (b'--frame\r\n'
-                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-        else:
-            yield (b'--frame\r\n'
-                            b'Content-Type: image/jpeg\r\n\r\n' + global_frame + b'\r\n\r\n')
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 @app.route('/video_viewer')
 def video_viewer():
-    return Response(video_stream(),
+    return Response(gen(VideoCamera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
  
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, threaded=True)
+    app.run()
