32a758c65a6a1a5c6045b55bc43158f30b186d20
Ted Huang <teddy57320@gmail.com>
2018-08-26 06:58:58 -0400

    fixed argument flag issues

--- src/main.py
+++ src/main.py
@@ -13,19 +13,19 @@ def main():
 	out_file = 'out.json'
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--demo', action='store_true')
-	parser.add_argument('--eye', action='store_true')
-	parser.add_argument('--emo', action='store_true')
-	parser.add_argument('--gcloud', action='store_true')
+	parser.add_argument('--eye', default=True, action='store_true')
+	parser.add_argument('--emo', default=True, action='store_true')
+	parser.add_argument('--gcloud', default=True, action='store_true')
 	parser.add_argument('--out_dir', default='../logs/')
 	args = parser.parse_args()
 
 	setup()
 	stats = StatsCollector()
 	interviewer = InterviewAI( stats, 
-							   is_demo=args.demo
-							   # run_eye_tracker=args.eye,
-							   # run_emo_threads=args.emo,
-							   # run_gcloud_threads=args.gcloud
+							   is_demo=args.demo,
+							   run_eye_tracker=args.eye,
+							   run_emo_threads=args.emo,
+							   run_gcloud_threads=args.gcloud
 							 )
 
 	interviewer.begin()
