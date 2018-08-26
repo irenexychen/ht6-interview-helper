062eed70efae6797ba1dcd03c819f340ccc6ecd9
Kevin Miao <wkmiao@edu.uwaterloo.ca>
2018-08-26 05:36:47 -0400

    centered video

--- basic-flask-app/camera.py
+++ basic-flask-app/camera.py
@@ -1,22 +1,44 @@
+
 import cv2
 
 class VideoCamera(object):
     def __init__(self):
-        # Using OpenCV to capture from device 0. If you have trouble capturing
-        # from a webcam, comment the line below out and use a video file
-        # instead.
-        self.video = cv2.VideoCapture(0)
-        # If you decide to use video.mp4, you must have this file in the folder
-        # as the main.py.
-        # self.video = cv2.VideoCapture('video.mp4')
+        # Open a camera
+        self.cap = cv2.VideoCapture(0)
+      
+        # Initialize video recording environment
+        self.is_record = False
     
     def __del__(self):
-        self.video.release()
+        self.cap.release()
     
     def get_frame(self):
-        success, image = self.video.read()
-        # We are using Motion JPEG, but OpenCV defaults to capture raw images,
-        # so we must encode it into JPEG in order to correctly display the
-        # video stream.
-        ret, jpeg = cv2.imencode('.jpg', image)
-        return jpeg.tobytes()
+        ret, frame = self.cap.read()
+
+        if ret:
+            ret, jpeg = cv2.imencode('.jpg', frame)
+
+            # Record video
+            # if self.is_record:
+            #     if self.out == None:
+            #         fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+            #         self.out = cv2.VideoWriter('./static/video.avi',fourcc, 20.0, (640,480))
+                
+            #     ret, frame = self.cap.read()
+            #     if ret:
+            #         self.out.write(frame)
+            # else:
+            #     if self.out != None:
+            #         self.out.release()
+            #         self.out = None  
+
+            return jpeg.tobytes()
+      
+        else:
+            return None
+
+    def start_record(self):
+        self.is_record = True
+
+    def stop_record(self):
+        self.is_record = False
--- basic-flask-app/routes.py
+++ basic-flask-app/routes.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, Response, jsonify, request
 from camera import VideoCamera
 import cv2
 
+video_camera = None
+global_frame = None
 
 app = Flask(__name__)
 
@@ -9,16 +11,38 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', the_title='ElevAIte')
 
-def gen(camera):
+@app.route('/demo_status', methods=['POST'])
+def demo_status():
+    global video_camera 
+    if video_camera == None:
+        video_camera = VideoCamera()
+ 
+    json = request.get_json()
+ 
+    status = json['status']
+ 
+    if status == "true":
+        video_camera.start_record()
+        return jsonify(result="started")
+    else:
+        video_camera.stop_record()
+        return jsonify(result="stopped")
+
+def video_stream():
+    global video_camera 
+  
+    if video_camera == None:
+        video_camera = VideoCamera()
+        
     while True:
-        frame = camera.get_frame()
+        frame = video_camera.get_frame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 @app.route('/video_viewer')
 def video_viewer():
-    return Response(gen(VideoCamera()),
+    return Response(video_stream(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
  
 if __name__ == '__main__':
-    app.run()
+    app.run(host='127.0.0.1', debug=True, threaded=True)
